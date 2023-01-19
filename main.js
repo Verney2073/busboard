d44f6474a51076da0b77adf0663a5006071b35ba
James Tribe <jjtribe93@gmail.com>
2023-01-19 12:21:11 +0000

    simplified by Emily, filter() removed, sorting earlier

--- main.js
+++ main.js
@@ -2,16 +2,15 @@
 
 const response = await fetch('https://api.tfl.gov.uk/StopPoint/?lat=51.553935&lon=-0.144754&stopTypes=NaptanPublicBusCoachTram');
 const busBoard = await response.json();
-const stoppingPoints = busBoard.stopPoints;
+const stoppingPoints = busBoard.stopPoints.sort((stopA, stopB) => stopA.distance - stopB.distance);
 
 //console.log(stoppingPoints.map(points => console.log(`Bus stop' ${points.commonName} is ${points.distance}m away`)));
 
-const stoppingDistances = stoppingPoints.map(points => points.distance).sort((a, b) => a - b);
-const closestStop = stoppingPoints.filter(points => points.distance === stoppingDistances[0]);
-const secondClosestStop = stoppingPoints.filter(points => points.distance === stoppingDistances[1]);
+const closestStop = stoppingPoints[0];
+const secondClosestStop = stoppingPoints[1];
 
-console.log(`the closest stop is ${Math.trunc(closestStop[0].distance)}m away`);
-console.log(`the second closest stop is ${Math.trunc(secondClosestStop[0].distance)}m away`);
+console.log(`the closest stop is ${closestStop.commonName} which is ${Math.trunc(closestStop.distance)}m away`);
+console.log(`the second closest stop is ${closestStop.commonName} which is ${Math.trunc(secondClosestStop.distance)}m away`);
 
 //All steps for busboard
 /* 0. Prompt the user to provide a postcode (optional)  JAMES 
