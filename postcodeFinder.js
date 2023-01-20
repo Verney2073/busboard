7db430e7a51178e2c944443ffa0fdcf5f3676794
James Tribe <jjtribe93@gmail.com>
2023-01-20 10:59:32 +0000

    error handling on postcode finder

--- postcodeFinder.js
+++ postcodeFinder.js
@@ -1,14 +1,27 @@
 //note readline below needs to be installed with npm on the command line before importing
 import readline from 'readline-sync';
 
+// OPTION 1 - Create a function to package up checking the formatting of the postcode 
+//function postCodeFormatter (postcode) {}
+
+// OPTION 2 - Create a do/while loop and handle the console log being different the first time the user inputs;
+
 
 export async function postcodeToCoords() {
+    let postcodeCheck = false;
+    let postcodeNoWS;
+
 
     console.log("Please enter your postcode:");
-    let postcode = readline.prompt();
+    do {
+        let postcode = readline.prompt();
+        postcodeNoWS = postcode.replace(/\s/g, "");
+        postcodeCheck = /^[a-z0-9]{5,7}$/i.test(postcodeNoWS);
+        if (!postcodeCheck) console.log("You entered an invalid postcode! Please try again or type 'exit' to exit");
+    } while (!postcodeCheck);
 
     try {
-        const postCodeResponse = await fetch(`https://api.postcodes.io/postcodes/${postcode}`);
+        const postCodeResponse = await fetch(`https://api.postcodes.io/postcodes/${postcodeNoWS}`);
         const postCodeJSON = await postCodeResponse.json();
         const latitude = postCodeJSON.result.latitude;
         const longitude = postCodeJSON.result.longitude;
@@ -16,7 +29,7 @@ export async function postcodeToCoords() {
         return coords;
     }
     catch {
-        console.log("It looks like you didn't enter a valid postcode!");
+        console.log("The API returned no results. Is this a valid postcode?");
     }
-};
+}
 
